dd3f607e974949ab665747d8e1ada16298ffa761
sudhir yadav <sk14395257@gmail.com>
2024-07-20 15:27:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-
+import Quora from './Components/Quora'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/counter/userSlice';
+import Login from './Components/Login';
+import { auth } from './firebase';
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        dispatch(login({
+          uid:authUser.uid,
+          displayName:authUser.displayName,
+          photo:authUser.photoURL,
+          email:authUser
+        }));
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+  },[dispatch]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user ? <Quora/> : <Login/>
+      }
     </div>
   );
 }
